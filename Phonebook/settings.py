157aa6f448fb6d59dfc72a9bd41bfe17de9d6839
kindyaknb <kindyak_nb@fizmat.tnpu.edu.ua>
2024-11-27 10:55:27 +0200

    tenth comm

--- Phonebook/settings.py
+++ Phonebook/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3p5_87d=*q*t!$d69ehdn3n88ml$rrp8*#w++1d!x5x*u07+@_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
